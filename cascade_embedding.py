02865a38527970daf096ef958ed9c1b8957f6b52
a.ghayouri <a.ghayouri@digikala.com>
2021-02-12 14:00:10 +0330

    iterate func

--- cascade_embedding.py
+++ cascade_embedding.py
@@ -1,9 +1,14 @@
 import os
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 def traverse_file():
-	directory = os.fsencode(os.path.join(ROOT_DIR, '../twitter-raw-data/Twitter/'))
+	directory = os.fsencode(os.path.join(ROOT_DIR, 'twitter-raw-data/Twitter/'))
 	for cascade_name in os.listdir(directory):
-		cascade_dir = os.fsencode(os.path.join(ROOT_DIR, '../twitter-raw-data/Twitter', cascade_name))
-		print(cascade_dir)
+		cascade_dir = os.fsencode(os.path.join(ROOT_DIR, 'twitter-raw-data/Twitter', str(cascade_name).split('\'')[1]))
+		for cascade in os.listdir(cascade_dir):
+			fd = open(os.path.join(str(cascade_dir).split('\'')[1], str(cascade).split('\'')[1]), 'r')
+			file_text = fd.read()
+			fd.close()
+			print(file_text)
+			
 
 traverse_file()
