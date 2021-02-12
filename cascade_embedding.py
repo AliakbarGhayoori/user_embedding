7d2cd63c81e605e13e7cdd16708332ce912370e4
a.ghayouri <a.ghayouri@digikala.com>
2021-02-12 13:43:38 +0330

    fix bug

--- cascade_embedding.py
+++ cascade_embedding.py
@@ -3,7 +3,7 @@ ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 def traverse_file():
 	directory = os.fsencode(os.path.join(ROOT_DIR, '../twitter-raw-data/Twitter/'))
 	for cascade_name in os.listdir(directory):
-            cascade_dir = os.fsencode(os.path.join(ROOT_DIR, '../twitter-raw-data/Twitter', cascade_name)
-	    print(cascade_dir)
+		cascade_dir = os.fsencode(os.path.join(ROOT_DIR, '../twitter-raw-data/Twitter', cascade_name))
+		print(cascade_dir)
 
 traverse_file()
