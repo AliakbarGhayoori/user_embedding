09caa35c1a17e045069683eba21b54d6c79e6ea1
a.ghayouri <a.ghayouri@digikala.com>
2021-02-13 13:25:15 +0330

    user text embedding

--- cascade_embedding.py
+++ cascade_embedding.py
@@ -2,10 +2,15 @@ import json
 import os
 from sentence_transformers import SentenceTransformer
 import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+import pickle
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+
+
 def embed_cascades_text():
-	model = SentenceTransformer('paraphrase-distilroberta-base-v1')
 	directory = os.fsencode(os.path.join(ROOT_DIR, 'twitter-raw-data/Twitter/'))
 	cascades_text_embeded = {}
 	for cascade_name in os.listdir(directory):
@@ -26,6 +31,50 @@ def embed_cascades_text():
 		cascades_text_embeded[str(cascade_name).split('\'')[1]] = cascade_embed
 	print(cascade_tweet_text)
 	return cascades_text_embeded
-			
+	
+	
+def embed_user_cascades(user_cascades):
+	fake_casc_percent = 0
+	user_tweets_embed = np.array([0 for _ in range(768)])
+	global model
+	
+	for cascade in user_cascades:
+		fake_casc_percent += cascade[1]
+		user_tweets_embed = np.add(user_tweets_embed, model.encode(cascade[2]))
+		
+	user_tweets_embed = user_tweets_embed / len(user_cascades)
+	fake_casc_percent = fake_casc_percent / len(user_cascades)
+	
+	return user_tweets_embed, fake_casc_percent
 
-embed_cascades_text()
+def user_embedding(users_dict : dict):
+	global model
+	users_bio = pd.DataFrame()
+	
+	user_ids = []
+	users_bio = None
+	users_tweets_embed = None
+	for user_id, user_info in users_dict.items():
+		user_ids += user_id
+		user_bio_embed = model.encode(user_info['description'])
+		user_tweets_embed, fake_casc_percent = embed_user_cascades(user_info['cascades_feature'])
+		
+		if users_bio is None:
+			users_bio  = [user_bio_embed.tolist()]
+			users_tweets_embed = [user_tweets_embed.tolist()]
+		else:
+			users_bio = np.append(users_bio, [user_bio_embed.tolist()], axis=0)
+			users_tweets_embed = np.append(users_tweets_embed, [user_tweets_embed.tolist()], axis = 0)
+		pca = PCA(n_components=200)
+		users_bio = pca.fit_transform(users_bio)
+		users_tweets_embed = pca.fit_transform(users_tweets_embed)
+		print(users_bio)
+		print(users_tweets_embed)
+
+user_dict = pickle.load(open( os.path.join(ROOT_DIR, '../../Representations/users_data.p'), "rb"))
+print(user_dict)
+user_embedding(user_dict)
+		
+	
+#user_embedding({'12': {'description':'hi to you', 'cascades_feature':[[12, 1, 'this is a test']]},'13': {'description':'hi to me', 'cascades_feature':[[12, 1, 'this is not a test']]}})
+		
