c4fde20afa592f806cd4b562d7b56997e7d38300
a.ghayouri <a.ghayouri@digikala.com>
2021-02-13 13:58:21 +0330

    user text embedding

--- cascade_embedding.py
+++ cascade_embedding.py
@@ -41,7 +41,7 @@ def embed_user_cascades(user_cascades):
 	global model
 	
 	for cascade in user_cascades:
-		fake_casc_percent += cascade[1]
+		fake_casc_percent += int(cascade[1])
 		user_tweets_embed = np.add(user_tweets_embed, model.encode(cascade[2]))
 		
 	user_tweets_embed = user_tweets_embed / len(user_cascades)
@@ -59,7 +59,8 @@ def user_embedding(users_dict : dict):
 	logging.info("start embedding.")
 	for user_id, user_info in users_dict.items():
 		user_ids += user_id
-		user_bio_embed = model.encode(user_info['description'])
+		print(user_info)
+		user_bio_embed = model.encode(user_info['profile_features']['description'])
 		user_tweets_embed, fake_casc_percent = embed_user_cascades(user_info['cascades_feature'])
 		
 		if users_bio is None:
@@ -68,12 +69,12 @@ def user_embedding(users_dict : dict):
 		else:
 			users_bio = np.append(users_bio, [user_bio_embed.tolist()], axis=0)
 			users_tweets_embed = np.append(users_tweets_embed, [user_tweets_embed.tolist()], axis = 0)
-		pca = PCA(n_components=200)
-		users_bio = pca.fit_transform(users_bio)
-		users_tweets_embed = pca.fit_transform(users_tweets_embed)
+	pca = PCA(n_components=200)
+	users_bio = pca.fit_transform(users_bio)
+	users_tweets_embed = pca.fit_transform(users_tweets_embed)
 		
-		logging.info("users bio: {0}".format(users_bio))
-		logging.info("users tweets embed".format(users_tweets_embed))
+	logging.info("users bio: {0}".format(users_bio))
+	logging.info("users tweets embed".format(users_tweets_embed))
 		
 user_dict = pickle.load(open( os.path.join(ROOT_DIR, '../../Representations/users_data.p'), "rb"))
 user_embedding(user_dict)
