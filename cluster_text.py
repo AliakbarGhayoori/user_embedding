894f31a004653e5c2046f038c3934b73fb810c77
a.ghayouri <a.ghayouri@digikala.com>
2021-02-26 11:37:54 +0330

    add making feature vector method.

--- cluster_text.py
+++ cluster_text.py
@@ -31,14 +31,13 @@ def embed_user_cascades(user_cascades):
 
 def user_embedding(users_dict: dict):
 	global model
-	
+	logging.info("start embedding.")
 	user_ids = []
 	users_bio = None
 	users_tweets_embed = None
 	logging.info("start embedding.")
 	for user_id, user_info in users_dict.items():
 		user_ids += [user_id]
-		print(user_info)
 		user_bio_embed = model.encode(user_info['profile_features']['description'])
 		user_tweets_embed, fake_casc_percent = embed_user_cascades(user_info['cascades_feature'])
 		
@@ -49,13 +48,53 @@ def user_embedding(users_dict: dict):
 			users_bio = np.append(users_bio, [user_bio_embed.tolist()], axis=0)
 			users_tweets_embed = np.append(users_tweets_embed, [user_tweets_embed.tolist()], axis=0)
 		
-	
-	pca = PCA(n_components=100)
-	users_bio = pca.fit_transform(users_bio)
-	users_tweets_embed = pca.fit_transform(users_tweets_embed)
-	
+	logging.info("start pca.")
+	bio_pca = PCA(n_components=50)
+	tweet_pca = PCA(n_components=100)
+	users_bio = bio_pca.fit_transform(users_bio)
+	logging.info("users pca finished.")
+	users_tweets_embed = tweet_pca.fit_transform(users_tweets_embed)
+	logging.info("tweets pca finished.")
 	return user_ids, users_bio, users_tweets_embed
 
+def users_feature_exctraction(users_ids, users_bio, users_tweet, my_data):
+	data_set = {}
+	logging.info("start making feature vectors of users.")
+	for i in range(len(users_ids)):
+		user = users_ids[i]
+		user_bio = users_bio[i].tolist()
+		user_tweet = users_tweet[i].tolist()
+		
+		profile_background_tile = 1 if my_data[user]['profile_features']['profile_background_tile'] else 0
+		profile_use_background_image = 1 if my_data[user]['profile_features']['profile_use_background_image'] else 0
+		screen_name = len(my_data[user]['profile_features']['screen_name'])
+		verified = 1 if my_data[user]['profile_features']['verified'] else 0
+		statuses_count = my_data[user]['profile_features']['statuses_count']
+		favourites_count = my_data[user]['profile_features']['favourites_count']
+		has_extended_profile = 1 if my_data[user]['profile_features']['has_extended_profile'] else 0
+		friends_count = my_data[user]['profile_features']['friends_count']
+		followers_count = my_data[user]['profile_features']['followers_count']
+		number_cascades = len(my_data[user]['cascades_feature'])
+		
+		user_feature_vect =[
+								 profile_background_tile,
+								 profile_use_background_image,
+								 screen_name,
+								 verified,
+								 statuses_count,
+								 favourites_count,
+								 has_extended_profile,
+								 friends_count,
+								 followers_count,
+								 number_cascades
+							 ] + user_bio + user_tweet
+		
+		data_set[user] = user_feature_vect
+		logging.info("user {0} added. index = {1}.".format(user, i))
+	logging.info('writing output to users_feature.p')
+	file_to_write = open('users_feature.p', 'wb')
+	pickle.dump(data_set, file_to_write)
+	logging.info("finished.")
 
 def users_clustering(users_ids, users_bio, users_tweet, my_data):
 	users_dataset = []
@@ -89,7 +128,7 @@ def users_clustering(users_ids, users_bio, users_tweet, my_data):
 							 ] + user_bio + user_tweet)
 	
 		data_set[i] = user
-		
+	
 	logging.info("making data matrix finished.")
 	
 	users_dataset = np.array(users_dataset)
@@ -103,9 +142,9 @@ def users_clustering(users_ids, users_bio, users_tweet, my_data):
 	with open('results1_text.txt', 'w') as f:
 		f.write("\n".join(str(result)))
 
-
-user_dict = pickle.load(open( os.path.join(ROOT_DIR, 'users_data.p'), "rb"))
-user_ids, users_bio, users_tweet = user_embedding(user_dict)
-users_clustering(user_ids, users_bio, users_tweet, user_dict)
+if __name__ == '__main__':
+	user_dict = pickle.load(open( os.path.join(ROOT_DIR, 'users_data.p'), "rb")) #give address to users_data.p here.
+	user_ids, users_bio, users_tweet = user_embedding(user_dict)
+	users_feature_exctraction(user_ids, users_bio, users_tweet, user_dict)
 
 # user_embedding({'12': {'description':'hi to you', 'cascades_feature':[[12, 1, 'this is a test']]},'13': {'description':'hi to me', 'cascades_feature':[[12, 1, 'this is not a test']]}})
